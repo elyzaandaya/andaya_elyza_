39da861386d3a7318ec368c39c9a0ef7643ff1f0
elyzaandaya <elyzaandaya750@gmail.com>
2025-10-07 07:19:23 +0800

    j

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://bacsa-jamesrodge.onrender.com/';
+$config['base_url'] 				= 'https://andaya-elyza.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
