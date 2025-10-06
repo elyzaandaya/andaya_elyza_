b98baa7f9978ae1f76e67b4e106a9872081db4af
elyzaandaya <elyzaandaya750@gmail.com>
2025-10-06 15:52:52 +0800

    jk

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12800432',
-    'password'	=> 'zXs38yrwFg' ,
-    'database'	=> 'sql12800432',
+    'username'	=> 'sql12800495',
+    'password'	=> '3RB4A1X8VQ' ,
+    'database'	=> 'sql12800495',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
